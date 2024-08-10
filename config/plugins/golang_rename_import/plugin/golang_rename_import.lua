2f2f8b0c5c78fd789d2c4d061ccf1271c527e479
back2nix <back2nix@list.ru>
2024-08-10 22:57:45 +0300

    delete comment

--- config/plugins/golang_rename_import/plugin/golang_rename_import.lua
+++ config/plugins/golang_rename_import/plugin/golang_rename_import.lua
@@ -1,50 +1,3 @@
--- local chan
-
--- local function ensure_job()
--- 	if chan then
--- 		return chan
--- 	end
--- 	chan = vim.fn.jobstart({ "golang_rename_import" }, { rpc = true })
--- 	return chan
--- end
-
--- local function rename_import(args)
--- 	local current_import = vim.fn.expand("<cword>")
-
--- 	local new_import = vim.fn.input("Enter the new import path: ", current_import)
--- 	print("new_import " .. new_import)
--- 	local result, err = vim.fn.rpcrequest(ensure_job(), "renameImport", { vim.fn.getcwd(), current_import, new_import })
--- 	print("after rpcrequest")
--- 	if err then
--- 		print("Error renaming import: " .. tostring(err))
--- 	else
--- 		print(result)
--- 		vim.cmd("bufdo e")
--- 	end
-
--- 	-- vim.ui.input({ prompt = "Enter the new import path: ", default = current_import }, function(new_import)
--- 	-- 	print("fuck fuck" .. new_import)
-
--- 	-- 	if not new_import or new_import == "" or new_import == current_import then
--- 	-- 		print("New import path must be different and non-empty")
--- 	-- 		return
--- 	-- 	end
-
--- 	-- 	print("fuck fuck 2" .. new_import)
-
--- 	-- 	local result, err = vim.fn.rpcrequest(ensure_job(), "renameImport", {vim.fn.getcwd(), current_import, new_import})
-
--- 	-- 	if err then
--- 	-- 		print("Error renaming import: " .. tostring(err))
--- 	-- 	else
--- 	-- 		print(result)
--- 	-- 		vim.cmd("bufdo e")
--- 	-- 	end
--- 	-- end)
--- end
-
--- vim.api.nvim_create_user_command("RenameImport", rename_import, { nargs = 0 })
-
 local function log(message)
 	local log_file = io.open(vim.fn.stdpath("data") .. "/golang_rename_import_plugin.log", "a")
 	if log_file then
@@ -135,37 +88,6 @@ local function rename_import()
 			vim.cmd("bufdo e")
 		end
 	end)
-
-	-- local new_import = vim.fn.input("Enter the new import path: ", old_import)
-	-- log("New import path entered: " .. new_import)
-
-	-- if new_import == "" or new_import == old_import then
-	-- 	log("Invalid new import path")
-	-- 	print("New import path must be different and non-empty")
-	-- 	return
-	-- end
-
-	-- log("Attempting to rename import from " .. old_import .. " to " .. new_import)
-
-	-- local job = ensure_job()
-	-- if not job then
-	-- 	log("Failed to ensure RPC server job")
-	-- 	print("Failed to start RPC server")
-	-- 	return
-	-- end
-
-	-- log("Sending RPC request")
-	-- local result, err = vim.rpcrequest(job, "renameImport", { project_root, old_import, new_import })
-	-- log("RPC request completed")
-
-	-- if err then
-	-- 	log("Error renaming import: " .. tostring(err))
-	-- 	print("Error renaming import: " .. tostring(err))
-	-- else
-	-- 	log("Import renamed successfully")
-	-- 	print(result)
-	-- 	vim.cmd("bufdo e") -- Reload all buffers to show changes
-	-- end
 end
 
 vim.api.nvim_create_user_command("RenameImport", rename_import, {})
