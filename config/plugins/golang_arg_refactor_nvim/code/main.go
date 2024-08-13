b0364de7c459f27716120cb77f4351fc9583f0d0
back2nix <back2nix@list.ru>
2024-08-13 03:24:05 +0300

    save main.go

--- config/plugins/golang_arg_refactor_nvim/code/main.go
+++ config/plugins/golang_arg_refactor_nvim/code/main.go
@@ -11,7 +11,9 @@ type Calculator struct {
 }
 
 func NewCalculator() *Calculator {
-	return &Calculator{cache: make(map[string]int)}
+	return &Calculator{
+		cache: make(map[string]int),
+	}
 }
 
 func (c *Calculator) Calculate(operation string, a, b int) int {
@@ -23,16 +25,22 @@ func (c *Calculator) Calculate(operation string, a, b int) int {
 	}
 	var result int
 	switch operation {
-	case "add":
+	case "1":
 		result = c.add(a, b)
-	case "multiply":
+	case "2":
 		result = c.multiply(a, b)
-	case "complexAdd":
-		result = c.complexOperationAdd(a, b, func(x, y int) int { return c.add(x, y) })
-	case "complexAdd2":
-		result = c.complexOperationAdd2(a, b, func(x, y int) int { return x + y })
+	case "3":
+		result = c.complex1(a, b, func(x, y int) int {
+			return c.add(x, y)
+		})
+	case "4":
+		result = c.complex2(a, b, func(x, y int) int {
+			return x + y
+		})
 	default:
-		result = c.complexOperation(a, b, func(x, y int) int { return x + y })
+		result = c.complex3(a, b, func(x, y int) int {
+			return x + y
+		})
 	}
 	c.cache[key] = result
 	return result
@@ -46,20 +54,29 @@ func (c *Calculator) multiply(x, y int) int {
 	return x * y
 }
 
-func (c *Calculator) complexOperationAdd2(a, b int, op func(int, int) int) int {
-	return c.add(a, b)
-}
-
-func (c *Calculator) complexOperationAdd(a, b int, op func(int, int) int) int {
+func (c *Calculator) complex1(
+	a, b int,
+	op func(int, int) int,
+) int {
 	return op(a, b)
 }
 
-func (c *Calculator) complexOperation(a, b int, op func(int, int) int) int {
+func (c *Calculator) complex2(
+	a, b int,
+	op func(int, int) int,
+) int {
+	return c.add(a, b)
+}
+
+func (c *Calculator) complex3(
+	a, b int,
+	op func(int, int) int,
+) int {
 	return op(a, b)
 }
 
 func main() {
 	calc := NewCalculator()
-	result := calc.Calculate("complexAdd2", 10, 20)
+	result := calc.Calculate("1", 10, 20)
 	fmt.Println("Result:", result)
 }
